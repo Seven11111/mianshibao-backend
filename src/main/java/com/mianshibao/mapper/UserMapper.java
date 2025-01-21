9a99045403b7976a3faa4371e3f29d46a6358f06
zhihuang <2584939303@qq.com>
2025-01-21 15:24:11 +0800

    初始化项目模板

---
+++ src/main/java/com/mianshibao/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.mianshibao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mianshibao.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
